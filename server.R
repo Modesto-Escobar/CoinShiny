0027817d734e3c10a8a00639a760ec527590c768
luismart <l.martinezuribe@gmail.com>
2015-07-17 12:58:32 +0200

    Signed-off-by: luismart <l.martinezuribe@gmail.com>

--- server.R
+++ server.R
@@ -8,7 +8,7 @@ shinyServer(function(input, output) {
   
   output$distPlot <- renderPlot({
     
-    # load the data and run coin
+    # load the data and run coin as we agreed
     load("Unamuno.RData")
     G<-gcoin(Unamuno, 1:31, UnamunoC, color=input$color, shape=input$shape, size=input$NodeSize, minimum=input$Minimum, p=as.numeric(input$pValue), lwidth=input$EdgeSize, layout=input$layout)
   })
